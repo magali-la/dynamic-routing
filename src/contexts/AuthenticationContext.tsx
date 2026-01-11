59c6f6945d363afba5a930e84dcaa743ae8ba0a9
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-11 15:49:11 -0500

    feat(auth context): add auth context and provider with isAuthenticated state and login/logout functions, wrap app in auth provider

--- src/contexts/AuthenticationContext.tsx
+++ src/contexts/AuthenticationContext.tsx
@@ -1,6 +1,37 @@
+import { createContext, useState } from "react";
+
 // set up type for auth context with demo boolean auth state and provider functions
 export interface AuthContextTypes {
     isAuthenticated: boolean;
     login: () => void;
     logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextTypes | undefined>(undefined);
+
+// set up provider with functions
+export const AuthProvider = ({ children }: { children: React.ReactNode}) => {
+    // set up auth state
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    // set up provider functions
+    function login() {
+        setIsAuthenticated(true);
+    }
+    
+    function logout() {
+        setIsAuthenticated(false);
+    }
+
+    const value: AuthContextTypes = {
+        isAuthenticated,
+        login,
+        logout
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
