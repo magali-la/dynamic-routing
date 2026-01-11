fd9ba9e32d32233d12c6301d6c398ee27b152c50
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-11 16:28:59 -0500

    feat(login): add login provider function to update auth state, create custom useAuth hook to verify context can be used on import and remove ts error

--- src/contexts/AuthenticationContext.tsx
+++ src/contexts/AuthenticationContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 // set up type for auth context with demo boolean auth state and provider functions
 export interface AuthContextTypes {
@@ -34,4 +34,13 @@ export const AuthProvider = ({ children }: { children: React.ReactNode}) => {
             {children}
         </AuthContext.Provider>
     )
+}
+
+// add custom Hook to ensure the components using the hook are wrapped in auth provider and show error if not - in this case, everything is wrapped by auth provider in main.tsx, but this also helps clear errors with ts when importing the context because the type could be undefined. this hook just checks if the context is able to be used or not (undefined or not and returns it wherever you are importing it)
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context){
+        throw Error('useAuth needs to be wrapped by AuthProvider to be used')
+    }
+    return context;
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,15 @@
+import { useAuth } from "../contexts/AuthenticationContext"
+
 export default function Login() {
+    // call custom hook to avoid ts error which checks if the context is available or not without having to use nonnull assertion
+    const { login } = useAuth();
 
     return (
         <>
             <h1>Login Page</h1>
             {/* simulated login */}
             <input value="DemoUser"></input>
-            <button>Login</button>
+            <button onClick={login}>Login</button>
         </>
     )
 }
