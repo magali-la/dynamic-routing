832c2fb4d73e5cd6728bbbe22502d0f78ab29169
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-11 17:15:05 -0500

    feat(login): add login handler to redirect to admin page after login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,26 @@
+import { useNavigate } from "react-router";
 import { useAuth } from "../contexts/AuthenticationContext"
 
 export default function Login() {
     // call custom hook to avoid ts error which checks if the context is available or not without having to use nonnull assertion
     const { login } = useAuth();
+    // import useNavigate hook to use in handler
+    const navigate = useNavigate();
+
+    // create login button handler which updates auth state + redirects
+    function handleLogin() {
+        // update the state to true
+        login();
+        // redirect with navigate to the admin dashboard
+        navigate("/admin");
+    }
 
     return (
         <>
             <h1>Login Page</h1>
             {/* simulated login */}
             <input value="DemoUser"></input>
-            <button onClick={login}>Login</button>
+            <button onClick={handleLogin}>Login</button>
         </>
     )
 }
