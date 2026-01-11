d57be73f475470f33067ef6bb2db0b4a80819945
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-11 14:50:44 -0500

    feat(blog index): import and map posts data into a list with a relative link to the slug of each post

--- src/pages/BlogIndex.tsx
+++ src/pages/BlogIndex.tsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router";
+import { posts } from "../lib/posts";
 
 export default function BlogIndex() {
 
     return (
         <>
-            <h1>blog index page</h1>
-            <Link to="/blog/post1">post 1 sample</Link>
-            <Link to="/blog/post2">post 2 sample</Link>
+            <h1>Recent Blogs</h1>
+            <ul>
+                {/* map posts into a list item with a link */}
+                {posts.map((post) => (
+                    // set a key for the item
+                    <li key={post.id}>
+                        {/* the link is a relative path without a forward slash so react knows this link is relative to where user currently is in the /blog path for BlogIndex being the "home" of that path */}
+                        <Link to={post.slug}>{post.title}</Link>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
